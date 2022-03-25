47d8ae60223c020ad6d772a3e22889d983a5d1f8
samed.uluboylu@gmail.com <samed.uluboylu@gmail.com>
2022-03-25 13:08:27 +0300

    25.03.2022 13:08

---
+++ DataAccess/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T entity)
+        {
+            using var p = new Context();
+            p.Remove(entity);
+            p.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var p = new Context();
+            return p.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var p = new Context();
+            return p.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            using var p = new Context();
+            p.Add(entity);
+            p.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var p = new Context();
+            p.Update(entity);
+            p.SaveChanges();
+        }
+    }
+}
